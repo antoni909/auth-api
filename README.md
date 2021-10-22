74a03c3b168f0933bf95388513c7527c0cc6fa16
antoni <antoni.dev0@gmail.com>
2021-10-22 14:59:14 -0700

    completes task 2, create new set of protected API routes for lab 8

---
+++ README.md
@@ -0,0 +1,19 @@
+# user
+
+{
+  "user": {
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN1cGVyX3VzZXIiLCJpYXQiOjE2MzQ5MTQzMzR9.mZxq2mvb9h27MbljhkT554x9Ui1O6pl6OzJqDYGBEKg",
+    "capabilities": [
+      "read",
+      "create",
+      "update",
+      "delete"
+    ],
+    "id": 6,
+    "username": "super_user",
+    "password": "$2b$10$mEL9dglX8OUWBiBoiUHSL.ISQxXOqr3cbSgW7Fqpp8z85fr6tiScm",
+    "role": "admin",
+},
+  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN1cGVyX3VzZXIiLCJpYXQiOjE2MzQ5MTQzMzR9.mZxq2mvb9h27MbljhkT554x9Ui1O6pl6OzJqDYGBEKg"
+}
+
--- src/api/auth/middleware/basic.js
+++ src/api/auth/middleware/basic.js
@@ -1,24 +1,25 @@
 'use strict';
 
 const base64 = require('base-64');
-const { users } = require('../models/indexAuth');
-
+const { users } = require('../../models/index');
+console.log('*** basic auth users:',users)
 module.exports = async (req, res, next) => {
 
   if (!req.headers.authorization) { return _authError(); }
 
   let basic = req.headers.authorization.split(' ').pop();
-  let [user, pass] = base64.decode(basic).split(':');
+  let [username, password] = base64.decode(basic).split(':');
 
   try {
-    req.user = await users.authenticateBasic(user, pass)
+    req.user = await users.authenticateBasic(username, password)
+    // console.log('*** basic auth req.user',req.user);
     next();
   } catch (e) {
     _authError()
   }
 
   function _authError() {
-    res.status(403).send('Invalid Login');
+    res.status(403).send('Basic Auth Invalid Login');
   }
 
 }
